869bf43fa7b76dce07ecda4379f1ae98111a0c47
Gino Fabrizio <51064212+SirTortuga333@users.noreply.github.com>
2024-11-28 10:17:29 -0300

    Added visual indication of who won and point counter TO-DO Game ends after a player gets to 5 points

--- javascript.js
+++ javascript.js
@@ -32,43 +32,108 @@ function getComputerChoice(){
 function playRound(HumanChoice, ComputeChoice){
     // Compare choices
 
-    const results = document.querySelector('.results');
-
+    const results = document.querySelector('#round-result');
+    //const points = Array.from(document.querySelectorAll("p")); // Index 0 is Human, index 1 is Computer
+    const points = document.querySelectorAll("p"); // Index 0 is Human, index 1 is Computer
 
     if(HumanChoice === 'ROCK'){
         if(ComputeChoice === 'SCISSORS'){
             console.log('You win! Rock beats Scissors.');
             humanScore++;
+            
+            if(results.classList.contains("lose")){
+                results.classList.toggle("lose"); // Desactiva lose
+                results.classList.toggle("win"); // Activa win
+            }
+            
+            results.textContent = 'You win! Rock beats Scissors.';
+            points[0].textContent = humanScore;
+
         } else if(ComputeChoice === 'PAPER'){
             console.log('You lose! Paper beats Rock.');
             computerScore++;
+
+            if(results.classList.contains("win")){
+                results.classList.toggle("lose"); // Activa lose
+                results.classList.toggle("win"); // Desctiva win
+            }
+            
+            results.textContent = 'You lose! Paper beats Rock.';
+            points[1].textContent = computerScore;
+
         } else {
             console.log("It's a tie! Both choose Rock!");
             ties++;
+
+            results.textContent = "It's a tie! Both choose Rock!";
+
         }
     } else if (HumanChoice === 'PAPER'){
         if(ComputeChoice === 'ROCK'){
             console.log('You win! Paper beats Rock.');
             humanScore++;
+
+            if(results.classList.contains("lose")){
+                results.classList.toggle("lose"); // Desactiva lose
+                results.classList.toggle("win"); // Activa win
+            }
+            
+            results.textContent = 'You win! Paper beats Rock.';
+            points[0].textContent = humanScore;
+
         } else if(ComputeChoice === 'SCISSORS'){
             console.log('You lose! Scissors beats Paper.');
             computerScore++;
+
+            if(results.classList.contains("win")){
+                results.classList.toggle("lose"); // Activa lose
+                results.classList.toggle("win"); // Desctiva win
+            }
+            
+            results.textContent = 'You lose! Scissors beats Paper.';
+            points[1].textContent = computerScore;
+
         } else {
             console.log("It's a tie! Both choose Paper!");
             ties++;
+
+            results.textContent = "It's a tie! Both choose Paper!";
+
         }
     } else { // Choose Scissors
         if(ComputeChoice === 'PAPER'){
             console.log('You win! Scissors beats Paper.');
             humanScore++;
+
+            if(results.classList.contains("lose")){
+                results.classList.toggle("lose"); // Desactiva lose
+                results.classList.toggle("win"); // Activa win
+            }
+            
+            results.textContent = 'You win! Scissors beats Paper.';
+            points[0].textContent = humanScore;
+
         } else if(ComputeChoice === 'Rock'){
             console.log('You lose! Rock beats Paper.');
             computerScore++;
+
+            if(results.classList.contains("win")){
+                results.classList.toggle("lose"); // Activa lose
+                results.classList.toggle("win"); // Desctiva win
+            }
+            
+            results.textContent = 'You lose! Rock beats Paper.';
+            points[1].textContent = computerScore;
+
         } else {
             console.log("It's a tie! Both choose Scissors!");
             ties++;
+
+            results.textContent = "It's a tie! Both choose Scissors!";
+
         }
     }
+
 }
 
 /* function playGame(){
